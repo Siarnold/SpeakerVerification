65933b47752bb8270fa0f11508dd8378de0b953d
Siarnold <siarnold@foxmail.com>
2017-05-12 01:01:39 +0800

    finish data_preprocess.py

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # A starting research into the applications of signals and systems.
 ------
+* data_process.py is used to preprocess the data used in the CNN
+* spv(= Speaker Verification)_cnn.py is used to formulate the network under keras
+
 NOT FINISHED.
--- data_process.py
+++ data_process.py
@@ -4,20 +4,43 @@
 
 import os
 import wave
+import numpy as np
 
-fpath = './data/ty/'# file path
-pfpath = './data/pty/'# processed file path
+fpath = './data/ty/' # file path
+pfpath = './data/pty/' # processed file path
+samRate = 4000 # sample rate
+samTime = 4 # sample time
+
+pfpath1 = pfpath + 'wavs/'
+pfpath2 = pfpath + 'dats/'
+nSamFrame = samRate * samTime # number of sample frames
 # isdir/isfile/exists
+# convert formats to wave
 if not os.path.isdir(pfpath):
 	os.mkdir(pfpath)
-	for fname in os.listdir(fpath):# file name
+	os.mkdir(pfpath1)
+	os.mkdir(pfpath2)
+	for fname in os.listdir(fpath): # file name
 		audif = fpath + fname
-		audipf = pfpath + fname[0:-4] + '.wav'
-		print(audipf)
-		str = 'ffmpeg -i ' + audif + ' -ar 2000 ' + audipf
-		os.system(str)
+		audipf = pfpath1 + fname[0:-4] + '.wav'
+		# convert format with sample rate and channel 1
+		cstr = 'ffmpeg -i ' + audif + ' -ar ' + str(samRate) + ' -ac 1 ' + audipf # command string
+		os.system(cstr)
 
-# the audio file
-# audif = filepath + filename
-# audi = wave.open(audif)
-# print(audi.getframerate(), audi.getnframes())
+# split the wave files and save as .dat files
+for wname in os.listdir(pfpath1): # wave name
+	audi = wave.open(pfpath1 + wname) # audio
+	nframes = audi.getnframes()
+	sdata = audi.readframes(nframes) # string type of data
+	audi.close()
+	wdata = np.fromstring(sdata, dtype = np.short) # short-type data, 2 Bytes
+	nsplits = nframes // 16000 # number of valid splits (with 16000 frames), whether the remaider is 0 or else
+	splits = np.split(wdata, [16000 * x + 1 for x in range(1, nsplits + 1)])
+	for x in range(nsplits):
+		split = splits[x]
+		split = np.fft.fft(split) # FFT to extract raw feature
+		split.tofile(pfpath2 + wname[0:-4] + '_{:0>4d}.dat'.format(x))
+		print(split.shape)
+
+# wdata = np.fromfile(pfpath2 + wname[0:-4] + '.dat', dtype = np.short)
+
